5f355e48bcf1a49212071bc110ea6e6ae98c95f6
Matheus-O-Silva <72843539+Matheus-O-Silva@users.noreply.github.com>
2021-02-17 21:31:16 -0300

    first commit

---
+++ home.php
@@ -0,0 +1,39 @@
+<php?
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="styles/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div>
+                // Aqui vai ficar o Logo da empresa
+            </div>
+            <nav class="nav-bar">
+                <ul>
+                    <li><a href="#sobre">Sobre</li>
+                    <li><a href="#portfolio">Porfólio</li>
+                    <li><a href="#produtos">Produtos</li>
+                    <li><a href="#contatos">Contato</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <!-- <section id="sobre">Sobre</section>
+        <section id="portfolio">Portfólio</section>
+        <section id="produtos">Produtos</section>
+        <section id="contatos">Contatos</section> -->
+    </main>
+    <img src="img/login_background">
+    <script type="javascript" src="scripts/script.js"></script>
+</body>
+</html>
+
---
+++ index.php.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once "classes/conexao.php";
+	$obj = new conectar();
+	$conexao = $obj->conexao();
+
+	$sql = "SELECT * from usuarios where email='admin'";
+	$result = mysqli_query($conexao, $sql);
+
+	$validar = 0;
+	if(mysqli_num_rows($result) > 0){
+		$validar = 1;
+    }
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.css">
+    <script src="lib/jquery-3.2.1.min.js"></script>
+	<script src="js/funcoes.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-image:url(img/pexels.jpg); width:100%; height:100px;">                       
+    <br><br><br>
+        <div class="container">
+            <div class="row" id="painel">  
+                <div class="col-sm-8"></div>
+                <div class="col-sm-8">
+				<div class="panel panel-primary">
+					<div class="panel panel-heading"><span id="portal">Portal</span></div>
+					<div class="panel panel-body">
+						<form id="frmLogin">
+							<label>Email</label>
+							<input type="text" class="form-control input-sm" name="email" id="email">
+							<label>Senha</label>
+							<input type="password" name="senha" id="senha" class="form-control input-sm">
+							<p></p>
+							<span class="btn btn-primary btn-sm" id="entrarSistema">Entrar</span>
+							<?php if(!$validar): ?>
+
+
+							<?php 
+								endif;
+							 ?>
+							
+						</form>
+					</div>
+				</div>
+			</div>
+
+            </div>
+        </div>
+
+</body>
+</html>
